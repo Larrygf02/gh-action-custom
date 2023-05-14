68d564ea89feb1dfba41bdffdcbd64d2cb9d7495
Larrygf02 <raulgf_02@hotmail.com>
2023-05-14 15:14:12 -0500

    add url from website

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -5,6 +5,7 @@ const exec = require('@actions/exec')
 function run() {
 
     // 1) Get inputs
+    core.notice('Starting Deploy action')
     const bucket = core.getInput('bucket', { required: true });
     const bucketRegion = core.getInput('bucket-region', { required: true });
     const distFolder = core.getInput('dist-folder', { required: true });
@@ -14,7 +15,10 @@ function run() {
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`); 
 
-    core.notice('Hello from my custom Javascript action')
+    const websiteUrl = `https://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+    core.setOutput('website-url', websiteUrl)
+
+    
 }
 
 run();
